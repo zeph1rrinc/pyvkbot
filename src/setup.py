5f1c55dc3c1c2400dc13c5f50fc33a4a3c1e1700
Zeph1rr <grianton535@gmail.com>
2022-12-06 01:45:24 +0300

    version 1.0.5

--- src/setup.py
+++ src/setup.py
@@ -17,7 +17,7 @@ else:
 
 setup(name='PyVkBot',
       python_requires='>3.10',
-      version='1.0.4',
+      version='1.0.5',
       description='Chat bot for vk.com',
       packages=['PyVkBot'],
       author='zeph1rr',
